f5e876a765e3dbfa7d029fa85dded037319ffe54
ohmyjons <dohanpw@gmail.com>
2020-03-13 15:48:14 +0700

    FIX

---
+++ soal2/soal2.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <string.h>
+
+int main() {
+  pid_t pid, sid;        // Variabel untuk menyimpan PID
+
+  pid = fork();     // Menyimpan PID dari Child Process
+
+  /* Keluar saat fork gagal
+  * (nilai variabel pid < 0) */
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  /* Keluar saat fork berhasil
+  * (nilai variabel pid adalah PID dari child process) */
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  while (1) {
+    
+    time_t t;
+    struct tm *tm;
+    char get_time[25];
+    time(&t);
+    tm = localtime(&t);
+    strftime(get_time,sizeof(get_time),"%Y-%M-%d_%X",tm);
+
+
+    struct stat st = {0};
+    char dir[100] = "/home/dohan/sisop20/modul2/soal/";
+    
+    strcat(dir,get_time);
+    int result = mkdir(dir, 0777);
+
+    
+
+
+    sleep(30);
+  }
+}
